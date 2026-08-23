9adeb4670b09a20197dd7ff93bb9a9eae8d7c76e
Oleg Pustovit <me@opustovit.com>
2026-08-24 01:45:53 +0200

    metal: exercise heap acceleration safety

--- lib/metal/test_metal_resource100_safe.ml
+++ lib/metal/test_metal_resource100_safe.ml
@@ -14,6 +14,11 @@ let ()=
  get(Resource100.Sample_attachment.destroy a);
  for i=1 to 10000 do let x=get(Resource100.View_pool_descriptor.create~label:(string_of_int i)~count:4L())in if Resource100.View_pool_descriptor.count x<>4L then failwith"descriptor count";get(Resource100.View_pool_descriptor.destroy x)done;
  let device=get(Device.system_default())in let desc=get(Resource100.View_pool_descriptor.create~count:4L())in
+ let heap=get(Heap.create~device(Heap.make_descriptor~size:1048576L()))in
+ reject(Resource100.Heap_ops.create_acceleration_structure heap~size:0L);
+ (match Resource100.Heap_ops.create_acceleration_structure heap~size:4096L with
+  |Ok acceleration->get(Acceleration_structure.destroy acceleration)
+  |Error _->());
  let texture=get(Texture.create~device(Texture.descriptor_2d~storage:Buffer.Shared~usage:[Texture.Shader_read;Texture.Shader_write]~format:Texture.Rgba8_unorm~width:4~height:4()))in
  if get(Resource100.Resource_ops.device(Resource100.Resource_ops.Texture texture))!=device then failwith"resource device";
  (match get(Resource100.Resource_ops.heap(Resource100.Resource_ops.Texture texture))with None->()|Some _->failwith"device texture heap");
@@ -42,4 +47,4 @@ let ()=
  reject(Resource100.Texture_ops.get_bytes texture~bytes:(Bytes.create 8)~bytes_per_row:16~mip_level:0~region:{x=0;y=0;z=0;width=4;height=1;depth=1});
  (match Resource100.Texture_view_pool.create device desc with Ok pool->if Resource100.Texture_view_pool.count pool<>4L then failwith"pool count";if get(Resource100.Texture_view_pool.checked_device pool)!=device then failwith"pool device";ignore(get(Resource100.Texture_view_pool.base_resource_id pool));ignore(get(Resource100.Texture_view_pool.label pool));reject(Resource100.Texture_view_pool.set_from_buffer pool~index:0 backing_buffer~offset:(-1L)~bytes_per_row:256(Texture.descriptor_2d~storage:Buffer.Shared~usage:[Texture.Shader_read]~format:Texture.Rgba8_unorm~width:4~height:1()));get(Resource100.Texture_view_pool.destroy pool)|Error _->());
  get(Texture.destroy backed);get(Buffer.destroy backing_buffer);
- get(Texture.destroy texture);get(Resource100.View_pool_descriptor.destroy desc);get(Device.destroy device)
+ get(Texture.destroy texture);get(Heap.destroy heap);get(Resource100.View_pool_descriptor.destroy desc);get(Device.destroy device)
