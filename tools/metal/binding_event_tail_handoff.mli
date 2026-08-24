ec30525ccc5538c3d02fbf832317ddfe7e6f0000
Oleg Pustovit <me@opustovit.com>
2026-08-24 02:24:06 +0200

    metal: audit event tail

---
+++ tools/metal/binding_event_tail_handoff.mli
@@ -0,0 +1,4 @@
+type package=Listener_constructor|Listener_queue|Shared_handle|Handle_label|Device_identity|Notification|Type_metadata
+type item={id:string;package:package;operation:string;tests:string list}
+val classify:kind:string->string->item
+val validate:item list->unit
