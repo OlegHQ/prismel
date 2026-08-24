b7d7cf2d58c1efbebc6e251b542413e39383369f
Oleg Pustovit <me@opustovit.com>
2026-08-24 02:22:10 +0200

    metal: audit indirect command tail

---
+++ tools/metal/binding_indirect_command_tail_handoff.mli
@@ -0,0 +1,4 @@
+type package=Compute_binding|Render_draw|Reset|Render_binding|Pipeline|Type_metadata
+type item={id:string;package:package;operation:string;tests:string list}
+val classify:kind:string->string->item
+val validate:item list->unit
