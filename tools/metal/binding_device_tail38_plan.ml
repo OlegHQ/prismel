55f52724e8ca8b2fe2e56f3662829ddb715dfb13
Oleg Pustovit <me@opustovit.com>
2026-08-27 10:44:42 +0200

    metal: pin exact device header tail38

---
+++ tools/metal/binding_device_tail38_plan.ml
@@ -0,0 +1,54 @@
+let owner_ids =
+  [ "method:-[MTLDevice areBarycentricCoordsSupported]"
+  ; "method:-[MTLDevice convertSparsePixelRegions:toTileRegions:withTileSize:alignmentMode:numRegions:]"
+  ; "method:-[MTLDevice convertSparseTileRegions:toPixelRegions:withTileSize:numRegions:]"
+  ; "method:-[MTLDevice counterSets]"
+  ; "method:-[MTLDevice getDefaultSamplePositions:count:]"
+  ; "method:-[MTLDevice maxThreadsPerThreadgroup]"
+  ; "method:-[MTLDevice newArgumentEncoderWithBufferBinding:]"
+  ; "method:-[MTLDevice newComputePipelineStateWithDescriptor:options:completionHandler:]"
+  ; "method:-[MTLDevice newComputePipelineStateWithFunction:completionHandler:]"
+  ; "method:-[MTLDevice newComputePipelineStateWithFunction:options:completionHandler:]"
+  ; "method:-[MTLDevice newComputePipelineStateWithFunction:options:reflection:error:]"
+  ; "method:-[MTLDevice newLibraryWithSource:options:completionHandler:]"
+  ; "method:-[MTLDevice newLibraryWithStitchedDescriptor:completionHandler:]"
+  ; "method:-[MTLDevice newRenderPipelineStateWithDescriptor:completionHandler:]"
+  ; "method:-[MTLDevice newRenderPipelineStateWithDescriptor:options:completionHandler:]"
+  ; "method:-[MTLDevice newRenderPipelineStateWithMeshDescriptor:options:completionHandler:]"
+  ; "method:-[MTLDevice newRenderPipelineStateWithTileDescriptor:options:completionHandler:]"
+  ; "method:-[MTLDevice newSharedEventWithHandle:]"
+  ; "method:-[MTLDevice queryTimestampFrequency]"
+  ; "method:-[MTLDevice sampleTimestamps:gpuTimestamp:]"
+  ; "method:-[MTLDevice setShouldMaximizeConcurrentCompilation:]"
+  ; "method:-[MTLDevice shouldMaximizeConcurrentCompilation]"
+  ; "method:-[MTLDevice sizeOfCounterHeapEntry:]"
+  ; "method:-[MTLDevice supportsCounterSampling:]"
+  ; "method:-[MTLDevice supportsFeatureSet:]"
+  ; "method:-[MTLDevice supportsRasterizationRateMapWithLayerCount:]"
+  ; "property:MTLDevice:barycentricCoordsSupported"
+  ; "property:MTLDevice:counterSets"
+  ; "property:MTLDevice:maxThreadsPerThreadgroup"
+  ; "property:MTLDevice:shouldMaximizeConcurrentCompilation"
+  ; "property:MTLDevice:supportsBCTextureCompression" ]
+
+let metadata_ids =
+  [ "class:MTLTilePipelineColorAttachmentDescriptor"
+  ; "function:MTLCopyAllDevicesWithObserver"
+  ; "function:MTLRemoveDeviceObserver"
+  ; "protocol:MTLIndirectComputeCommandEncoder"
+  ; "protocol:MTLIndirectRenderCommandEncoder"
+  ; "typedef:MTLDeviceNotificationHandler"
+  ; "typedef:MTLDeviceNotificationName" ]
+
+let async10 =
+  List.filter (fun id ->
+    String.ends_with ~suffix:"completionHandler:]" id)
+    owner_ids
+
+let validate () =
+  let all = owner_ids @ metadata_ids in
+  if List.length owner_ids <> 31 || List.length metadata_ids <> 7
+     || List.length all <> 38
+     || List.length (List.sort_uniq String.compare all) <> 38
+  then invalid_arg "Device MTLDevice.h tail38 drift";
+  if List.length async10 <> 10 then invalid_arg "Device async10 drift"
