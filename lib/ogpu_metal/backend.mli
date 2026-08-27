ea0eb6d4a23cb86d333b3ad9947362485ecda556
Oleg Pustovit <me@opustovit.com>
2026-08-28 01:54:51 +0200

    Cache native sampler descriptors

--- lib/ogpu_metal/backend.mli
+++ lib/ogpu_metal/backend.mli
@@ -1,3 +1,4 @@
 type control
 val create : ?device:Device.t -> ?layer:Metal.Metal_layer.t -> unit -> Ogpu.Backend.driver * control
 val register_pipeline : control -> Pipeline.t -> unit
+val sampler_cache_entries : control -> int
